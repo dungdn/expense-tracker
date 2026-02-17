996004e0dbd96991be011a1522090baf85fc2c7b
dungdn <ngocdungdinh@gmail.com>
2026-02-17 14:14:23 +0700

    add swagger ul (#5)
    add swagger ul (#5)

--- src/categories/create-category.dto.ts
+++ src/categories/create-category.dto.ts
@@ -1,10 +1,13 @@
 import { IsNotEmpty, IsString, IsOptional } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger'; // <--- Import
 
 export class CreateCategoryDto {
+  @ApiProperty({ example: "Ăn uống", description: "Tên danh mục" }) // <--- Thêm dòng này
   @IsNotEmpty() // Bắt buộc phải có
   @IsString()   // Phải là chuỗi
   name: string;
 
+  @ApiProperty({ example: 'Chi tiêu hằng ngày', required: false }) // <--- Thêm dòng này
   @IsOptional() // Không bắt buộc
   @IsString()
   description?: string;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common"; // <--- Import này
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // <--- 1. Import Swagger
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Kích hoạt Validation cho toàn bộ ứng dụng
   app.useGlobalPipes(new ValidationPipe());
+
+  // --- 2. Cấu hình Swagger ---
+  const config = new DocumentBuilder()
+    .setTitle('Expense Tracker API') // Tiêu đề
+    .setDescription('API quản lý chi tiêu cá nhân') // Mô tả
+    .setVersion('1.0') // Phiên bản
+    .addTag('Categories') // Các nhóm API (tùy chọn)
+    .addTag('Transactions')
+    .addTag('Reports')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Đường dẫn sẽ là /api
+  // ---------------------------
 
   await app.listen(3000);
 }
